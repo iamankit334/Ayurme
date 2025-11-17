dc56d9d81d37c65416a9e2dc8e816f28d9b904d6
iamankit334 <117710749+iamankit334@users.noreply.github.com>
2025-11-17 11:33:06 +0530

    second

--- ayurveda_project/settings.py
+++ ayurveda_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tgkbz*s(dkx%0^82b9qzijk-)t1kxp&f0(a++axb!li#*5s2gp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
